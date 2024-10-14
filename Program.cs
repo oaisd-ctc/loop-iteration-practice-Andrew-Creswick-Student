b5f09b65034033c02c10aa3b06bdf46989772761
Andrew-Creswick-Student <ancr131@student.oaisd.org>
2024-10-14 17:53:48 +0000

    Setup the basic methods for this program.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace loop_iteration_practice_Andrew_Creswick{
+    class Program{
+        static void Main(string[] args){
+            
+        }
+        static void Write1ThroughN_While(int n){
+
+        }
+        static void Write1ThroughN_For(int n){
+
+        }
+        static void WriteNThrough1_While(int n){
+            
+        }
+        static void WriteNThrough1_For(int n){
+            
+        }
+        static void WriteEvensThrough100(){
+            
+        }
+        static void FindSum(int n){
+
+        }
+        static void FindSumOfEvenNumbers(int n){
+            
+        }
+        static void FindSumOfOddNumbers(int n){
+            
+        }
+        static void OutputRightTriangle(int _base){
+            
+        }
+        static void OutOfRangeMessage(int n){
+            
+        }
+    }
+}
